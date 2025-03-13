dfd8058287da01c95692bec3a62cc9746a39bd2e
hazemezz123 <hazemezz988@gmail.com>
2025-03-13 19:48:51 +0200

    >>

--- PLRP/src/App.jsx
+++ PLRP/src/App.jsx
@@ -1,57 +1,8 @@
-import React from "react";
-
-function Button({ name = "undefined", title = "No title" }) {
+function App() {
   return (
     <>
-      <h1>{title}</h1>
-      <button className="btn btn-primary">{name}</button>
+      <h1>hello world</h1>
     </>
   );
 }
-function Layout({ children }) {
-  return (
-    <>
-      <main
-        style={{
-          margin: "10px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {children}
-      </main>
-    </>
-  );
-}
-function Card(props) {
-  return (
-    <>
-      <div className="card text-start">
-        <img
-          height={props.height}
-          className={`card`}
-          src={props.img}
-          alt={props.title}
-        />
-        <div className="card-body">
-          <h4 className="card-title">{props.title}</h4>
-          <p className="card-text">{props.name}</p>
-        </div>
-      </div>
-    </>
-  );
-}
-//  height . title . name . img
-const App = () => {
-  return (
-    <>
-      <Layout>
-        <Card height="100" title="Cute Cat" name="مشمشة" img="/img.png" />
-        <Card />
-      </Layout>
-    </>
-  );
-};
-
 export default App;
---
+++ PLRP/src/Project1/userCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const userCard = () => {
+  return <div>userCard</div>;
+};
+
+export default userCard;
